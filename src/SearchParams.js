6481b5b639c425f7d687cca382228b27865598c6
areyes107 <angelmrg2000@hotmail.com>
2020-12-28 13:19:01 -0600

    dropdown created, dropdown implemented to searchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,17 @@
 import { ANIMALS } from "@frontendmasters/pet";
 import React, { useState } from "react";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("Dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   console.log("state of location", location);
 
   return (
     <div className="search-params">
-      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
@@ -23,40 +23,8 @@ const SearchParams = () => {
           />
         </label>
 
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option> ALL </option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disabled={!breeds.lenght}
-          >
-            <option> ALL </option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
 
         <button>Submit</button>
       </form>
